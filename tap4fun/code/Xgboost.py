655f054562032a153982598859e8ff5e698f1d00
lj <779365135@qq.com>
2018-08-30 19:48:20 +0800

    tap4fun

--- tap4fun/code/Xgboost.py
+++ tap4fun/code/Xgboost.py
@@ -125,6 +125,6 @@ def xgboostPredict(testfile):
 
 
 if __name__ == '__main__':
-    xgboostparams()
-    # xgboostLinear()
+    # xgboostparams()
+    xgboostLinear()
     # xgboostPredict("../data/TapFunTest.csv")
